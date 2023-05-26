06ba0128b69a75dfa982509797d1cb0b7f6c58ac
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-27 02:59:22 +0600

    marquee added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,11 @@ import Footer from '../Shared/Footer/Footer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import RightNav from '../Shared/RightNav/RightNav';
 
 const Home = () => {
     return (
-        <div>
+        <div className='container'>
             <Header></Header>
             <Container>
                 {/* <Row>
@@ -16,13 +17,13 @@ const Home = () => {
                 </Row> */}
                 <Row>
                     <Col lg={3}>
-
+                        <RightNav></RightNav>
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main Content</h2>
                     </Col>
                     <Col lg={3}>
-                    <h2>Main Content</h2>
-                    </Col>
-                    <Col lg={3}>
-
+                        <RightNav></RightNav>
                     </Col>
                 </Row>
             </Container>
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
+import { Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div className='text-center'>
-            <img src={logo} alt="" />
-            <p className='text-secondary'>Journalism without fear or favor</p>
-            <p>{moment().format("dddd, MMMM Do YYYY")}</p>
-        </div>
+        <Container className='container'>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p className='text-secondary'>Journalism without fear or favor</p>
+                <p>{moment().format("dddd, MMMM Do YYYY")}</p>
+            </div>
+            <div className='d-flex'>
+                <div>
+                    <button className='btn btn-danger'>Latest</button>
+                </div>
+                <Marquee className='text-danger'>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 

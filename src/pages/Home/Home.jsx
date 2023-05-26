d3f081baa0e0240bf5d921f3494a14f929a3d79d
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-26 23:59:01 +0600

    fixed code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from '../Shared/Header/Header';
 import Footer from '../Shared/Footer/Footer';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Container>
+                <Row>
+                    <Col sm={8}>sm=8</Col>
+                    <Col sm={4}>sm=4</Col>
+                </Row>
+                <Row>
+                    <Col sm>sm=true</Col>
+                    <Col sm>sm=true</Col>
+                    <Col sm>sm=true</Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );

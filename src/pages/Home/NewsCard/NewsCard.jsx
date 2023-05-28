ae9d635d4cc74c712bb749d58ba0a8e294ada583
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-28 23:58:38 +0600

    ratings added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -3,7 +3,8 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 import moment from 'moment';
-import { FaBookmark, FaEye, FaShareAlt } from "react-icons/fa";
+import { FaBookmark, FaEye, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from 'react-rating';
 
 const NewsCard = ({ news }) => {
     const { _id, title, details, image_url, author, rating, total_view } = news;
@@ -18,7 +19,7 @@ const NewsCard = ({ news }) => {
                     </div>
                     <div>
                         <FaBookmark />
-                        <FaShareAlt/>
+                        <FaShareAlt />
                     </div>
                 </Card.Header>
                 <Card.Body>
@@ -30,8 +31,21 @@ const NewsCard = ({ news }) => {
                         }
                     </Card.Text>
                 </Card.Body>
-                <Card.Footer className="text-muted">
-                        <FaEye/> {total_view}
+                <Card.Footer className="text-muted d-flex">
+                    <div className='flex-grow-1'>
+                        <Rating>
+                            <Rating
+                                placeholderRating={3.5}
+                                emptySymbol={<FaStar></FaStar>}
+                                placeholderSymbol={<img src="assets/images/star-red.png" className="icon" />}
+                                fullSymbol={<img src="assets/images/star-yellow.png" className="icon" />}
+                            />
+                        </Rating>
+                        {rating?.number}
+                    </div>
+                    <div>
+                        <FaEye /> {total_view}
+                    </div>
                 </Card.Footer>
             </Card>
         </div>
